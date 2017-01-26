f1344fde83112d1f850a12b95b8f08293fe80745
Cody Mercer <cklmercer@users.noreply.github.com>
2017-01-26 16:57:14 -0600

    update formatting

--- app/components/test.vue
+++ app/components/test.vue
@@ -4,20 +4,13 @@
     </div>
 </template>
 
-<script type="text/babel">
+<script>
     export default {
         name: 'test',
-
         store: ['message'],
-
         mounted() {
-            setTimeout(() => {
-                this.$events.fire('testEvent', 'You just received a message from test.vue')
-            }, 1500)
-
-            setTimeout(() => {
-                this.$events.fire('testTwo', 'hahah you got another message')
-            }, 5000)
+            setTimeout(() => this.$events.fire('testEvent', 'You just received a message from test.vue'), 1500)
+            setTimeout(() => this.$events.fire('testTwo', 'hahah you got another message'), 5000)
         }
     }
 </script>
